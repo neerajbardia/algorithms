08b957265a631a044d83a53ed103988a7470a066
neerajbardia <43133837+neerajbardia@users.noreply.github.com>
2020-12-06 21:15:26 +0530

    Create selection_sort.py

---
+++ sorting/selection_sort.py
@@ -0,0 +1,26 @@
+#selection sort with iteration count
+
+list_of_no=[]
+count=0
+scount=0
+no=int(input("Enter the no of values:"))
+for i in range(0,no):
+    count+=1
+    x=int(input("Enter no:"))
+    list_of_no.append(x)
+n=len(list_of_no)
+for i in range(0,n):
+    count+=1
+    min=list_of_no[i]
+    for j in range(i+1,n):
+        count+=1
+        if min>list_of_no[j]:
+            count+=1
+            min=list_of_no[j]
+            scount+=1
+            list_of_no[i],list_of_no[j]=list_of_no[j],list_of_no[i]
+
+
+print("Iteration Count:",count)
+print("No of times swapping happens:",scount)
+print("Sorted:",list_of_no)
